ecb561453fdde18b77a0b2e66b474fc3ef16fed5
steenstn <alest849@gmail.com>
2019-10-19 08:49:28 +0200

    import scripts

--- out/production/raytracerkotlin/raytracerkotlin.js
+++ out/production/raytracerkotlin/raytracerkotlin.js
@@ -1,3 +1,4 @@
+importScripts("https://steenstn.github.io/raytracerkotlin/out/production/raytracerkotlin/lib/kotlin.js")
 if (typeof kotlin === 'undefined') {
   throw new Error("Error loading module 'raytracerkotlin'. Its dependency 'kotlin' was not found. Please, check whether 'kotlin' is loaded prior to 'raytracerkotlin'.");
 }
